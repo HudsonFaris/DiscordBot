6245566bfaa5dfdc4a3d3674d82b8c7acea2ac49
HudsonFaris <hudsonfaris@gmail.com>
2026-09-26 09:44:38 -0400

    Dotenv before config declaration

--- recorder.js
+++ recorder.js
@@ -5,6 +5,9 @@ import path from 'path';
 import { execSync } from 'child_process';
 import { v2 as cloudinary } from 'cloudinary';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
